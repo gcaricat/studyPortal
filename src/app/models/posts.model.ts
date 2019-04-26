bc460613c1e657f972561a3148d54a729c4e5513
gcaricat@yahoo.it <fede333565245>
2019-04-26 23:34:47 +0200

    V2 APi posts and show the posts in dashboard - With Comments

--- src/app/models/posts.model.ts
+++ src/app/models/posts.model.ts
@@ -2,13 +2,20 @@
  * Posts class used to save the posts sended via api
  */
 export class Posts {
-    public publish: boolean;
+    public publish;
     public priority;
     public authId;
+    public authName;
     public content;
     public title;
 
-    constructor() {
+    constructor(publish, priority, authId, authName, content, title) {
+        this.publish = publish;
+        this.priority = priority;
+        this.authId = authId;
+        this.authName = authName;
+        this.content = content;
+        this.title = title;
     }
 }
 
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../api.service';
 import {Posts} from '../models/posts.model';
+import {post} from "selenium-webdriver/http";
+import {error} from "util";
 
 @Component({
   selector: 'app-posts',
@@ -12,20 +14,39 @@ export class PostsComponent implements OnInit {
    * Array that contain all posts
    */
   public listPosts: Posts[] = [];
+  public authName;
 
   constructor(private apiService: ApiService) {
+
+
+   // this.apiService.getUser().subscribe();
   }
 
   /**
    * Call the function getPosts to get all showed posts
    */
   ngOnInit() {
+    /*
     this.apiService.getPosts().subscribe( (postsData: Posts[]) => {
-      console.log(postsData);
       this.listPosts = postsData;
     });
+    */
+    this.apiService.getPosts().subscribe( (postsData: Posts[]) => {
+      console.log(postsData);
+      for(let item of postsData){
 
-    this.apiService.getUser().subscribe()
+        if ( item.authId ) {
+
+          this.apiService.getSingleAuthor(item.authId).subscribe( res =>{
+            this.authName = res.username;
+            const singlePost = new Posts(item.publish, item.priority, item.authId, this.authName, item.content, item.title);
+            this.listPosts.push(singlePost);
+          });
+        }
+      }
+    });
+
+
   }
 
   /**
@@ -33,11 +54,11 @@ export class PostsComponent implements OnInit {
    * @param userId
    */
   getAuthName(userId){
-    console.log("userid ", userId);
+
     if( userId != "undefined" && userId != "" ) {
       this.apiService.getSingleAuthor(userId).subscribe( res =>{
-        console.log(res.username);
-            return res.username;
+        res.username.toString();
+            // return res.username.toString();
           }
       );
     }
